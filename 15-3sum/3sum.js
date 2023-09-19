7cf6c565cf121e3eea019f3b124e3f56fa838e9d
meriamamri <51541705+meriamamri@users.noreply.github.com>
2023-09-19 20:50:52 +0100

    Time: 145 ms (82.29%) | Memory: 57.7 MB (97.12%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=> a-b)
+    let result = []
+    for(let i=0; i<nums.length-2; i++){
+        if(i>0 && nums[i] === nums[i-1]) continue;
+        let start = i+1
+        let end = nums.length-1
+        while(start<end){
+            if(nums[i]+nums[start]+nums[end]===0){
+                result.push([nums[i],nums[start],nums[end]])
+                while(start<end && nums[end]===nums[end-1]){
+                    end--
+                }
+                while(start<end && nums[start]===nums[start+1]){
+                    start++
+                }
+                start++
+                end--
+            }else if(nums[i]+nums[start]+nums[end]>0){
+                    end--
+            }else{
+                    start++
+                }
+        }
+    }
+    
+    return result
+};
