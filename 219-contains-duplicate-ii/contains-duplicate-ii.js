5b8b552dd448bb63bee0666a54ab4c3da259eb1d
meriamamri <51541705+meriamamri@users.noreply.github.com>
2023-10-01 23:18:00 +0100

    Time: 71 ms (96.30%) | Memory: 55.6 MB (75.50%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -3,7 +3,25 @@
  * @param {number} k
  * @return {boolean}
  */
+
 var containsNearbyDuplicate = function(nums, k) {
+    const numSet = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > k) {
+        numSet.delete(nums[i - k - 1]); // Remove the number outside the window
+        }
+
+        if (numSet.has(nums[i])) {
+        return true; // Found a duplicate within the window
+        }
+
+        numSet.add(nums[i]); // Add the current number to the set
+    }
+
+    return false;
+};
+/*var containsNearbyDuplicate = function(nums, k) {
     let DuplicateMap = new Map()
     for(let i=0; i<nums.length; i++){
         let j = DuplicateMap.get(nums[i])
@@ -16,4 +34,4 @@ var containsNearbyDuplicate = function(nums, k) {
     }
 
     return false
-};
+};*/
