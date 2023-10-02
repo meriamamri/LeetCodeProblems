b472be5dac71e08f1c6ada6837af0cc44105f44d
meriamamri <51541705+meriamamri@users.noreply.github.com>
2023-10-02 11:50:38 +0100

    Time: 53 ms (97.57%) | Memory: 41.6 MB (99.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,7 +3,29 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const charCount = new Array(26).fill(0); // Assuming only lowercase English letters
+
+    for (let i = 0; i < s.length; i++) {
+        charCount[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        charCount[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    for (let count of charCount) {
+        if (count !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/*var isAnagram = function(s, t) {
     let newS = s.split('').sort()
     let newT = t.split('').sort()
     let i=0
@@ -17,4 +39,4 @@ var isAnagram = function(s, t) {
     }
 
     return i===s.length && j===t.length
-}
+}*/
