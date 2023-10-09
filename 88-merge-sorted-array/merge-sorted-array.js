dea096c9b1cf8a305aa096e9172f020075967467
meriamamri <51541705+meriamamri@users.noreply.github.com>
2023-10-09 14:38:04 +0100

    Time: 50 ms (69.45%) | Memory: 42.2 MB (36.14%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let totalLength = m+n-1
+    n--
+    m--
+    while(n>=0){
+        if(m>=0 && nums1[m]>nums2[n]){
+            nums1[totalLength] = nums1[m]
+            m--
+        }else{
+            nums1[totalLength] = nums2[n]
+            n--
+        }
+        
+        totalLength--
+
+    }
+};
