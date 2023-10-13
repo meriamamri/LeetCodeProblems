df30a999c85eb263eac29909707b5a414b29c8c7
meriamamri <51541705+meriamamri@users.noreply.github.com>
2023-10-13 11:54:45 +0100

    Time: 90 ms (20.49%) | Memory: 52.1 MB (57.75%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -7,23 +7,18 @@ var rotate = function(nums, k) {
     k = k % nums.length;
 
     if (k === 0) {
-        return;  // No rotation needed
+        return; 
     }
 
-    // Reverse the first part of the array (from 0 to nums.length - k - 1)
-    console.log(k, nums.length - k - 1, nums.length - k)
     reverse(nums, 0, nums.length - k - 1);
 
-    // Reverse the second part of the array (from nums.length - k to nums.length - 1)
     reverse(nums, nums.length - k, nums.length - 1);
 
-    // Reverse the entire array
     reverse(nums, 0, nums.length - 1);
 };
 
 function reverse(nums, start, end) {
     while (start < end) {
-        // Swap elements at start and end indices
         const temp = nums[start];
         nums[start] = nums[end];
         nums[end] = temp;
