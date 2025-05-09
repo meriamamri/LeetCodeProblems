0120167cf22724b99b9c9192801fdc6a8e4c7648
meriamamri <51541705+meriamamri@users.noreply.github.com>
2025-05-10 00:32:25 +0100

    Time: 1196 ms (5.13%) | Memory: 79.4 MB (5.32%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max=0
+    let l = 0
+    let r = height.length-1
+
+    while(l<r){
+        let area = Math.min(height[r],height[l])*(r-l)
+        max = Math.max(max, area)
+
+        console.log(max, l, r, r-l, height[r]-height[l])
+
+        if(height[r]>height[l]){
+            l++
+        }else{
+            r--
+        }
+    }
+
+    return max
+};
