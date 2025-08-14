0bf005389d9a005c54be1ec00e375991cb9aec5c
meriamamri <51541705+meriamamri@users.noreply.github.com>
2025-08-14 12:54:13 +0100

    Time: 7 ms (90.91%) | Memory: 56 MB (88.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,40 +3,20 @@
  * @param {string} t
  * @return {boolean}
  */
-
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
+    if (s.length !== t.length) return false
+
+    let countArr = new Array(26).fill(0)
+
+    for(let i=0; i<s.length; i++){
+
+        countArr[s.charCodeAt(i)-'a'.charCodeAt(0)]++
+        countArr[t.charCodeAt(i)-'a'.charCodeAt(0)]--
     }
 
-    const charCount = new Array(26).fill(0); // Assuming only lowercase English letters
-
-    for (let i = 0; i < s.length; i++) {
-        charCount[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-        charCount[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    for(count of countArr){
+        if(count !==0) return false
     }
 
-    for (let count of charCount) {
-        if (count !== 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-/*var isAnagram = function(s, t) {
-    let newS = s.split('').sort()
-    let newT = t.split('').sort()
-    let i=0
-    let j=0
-    while(i<newS.length && j<newT.length){
-        if(newS[i]!==newT[j]){
-            return false
-        }
-        i++
-        j++
-    }
-
-    return i===s.length && j===t.length
-}*/
+    return true
+};
